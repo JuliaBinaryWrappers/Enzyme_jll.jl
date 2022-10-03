d1582b7b221188c035371c7517fb8f2fd96faa19
jlbuild <juliabuildbot@gmail.com>
2022-10-03 21:39:04 +0000

    Enzyme_jll build 0.0.40+0

--- .pkg/platform_augmentation.jl
+++ .pkg/platform_augmentation.jl
@@ -1,22 +1,22 @@
 using Base.BinaryPlatforms
-    function augment_llvm!(platform::Platform)
-        haskey(platform, "llvm_version") && return platform
+function augment_llvm!(platform::Platform)
+    haskey(platform, "llvm_version") && return platform
 
-        llvm_version = Base.libllvm_version
-        # does our LLVM build use assertions?
-        llvm_assertions = try
-            cglobal((:_ZN4llvm24DisableABIBreakingChecksE, Base.libllvm_path()), Cvoid)
-            false
-        catch
-            true
-        end
-        platform["llvm_version"] = if llvm_assertions
-            "$(llvm_version.major).asserts"
-        else
-            "$(llvm_version.major)"
-        end
-        return platform
+    llvm_version = Base.libllvm_version
+    # does our LLVM build use assertions?
+    llvm_assertions = try
+        cglobal((:_ZN4llvm24DisableABIBreakingChecksE, Base.libllvm_path()), Cvoid)
+        false
+    catch
+        true
     end
+    platform["llvm_version"] = if llvm_assertions
+        "$(llvm_version.major).asserts"
+    else
+        "$(llvm_version.major)"
+    end
+    return platform
+end
 
 function augment_platform!(platform::Platform)
     augment_llvm!(platform)
