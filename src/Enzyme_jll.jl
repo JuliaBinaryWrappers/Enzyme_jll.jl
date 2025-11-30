e540daf725059e590f912790c2750ac94813030f
jlbuild <juliabuildbot@gmail.com>
2025-11-30 15:54:14 +0000

    Enzyme_jll build 0.0.224+0

--- src/Enzyme_jll.jl
+++ src/Enzyme_jll.jl
@@ -7,5 +7,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Enzyme")
-JLLWrappers.@generate_main_file("Enzyme", UUID("7cc45869-7501-5eee-bdea-0790c847d4ef"))
+JLLWrappers.@generate_main_file("Enzyme", Base.UUID("7cc45869-7501-5eee-bdea-0790c847d4ef"))
 end  # module Enzyme_jll
